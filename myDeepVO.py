ceb2cb591d86484b35046f5710dbfdba72b9b09c
S <allenhsu1234@hotmail.com>
2019-06-22 14:49:00 +0800

    revise model

--- myDeepVO.py
+++ myDeepVO.py
@@ -90,7 +90,16 @@ class myDeepVo(nn.Module):
         out = self.classifier(out)
 
         return out  # batch * frame * 6個參數 theta , x y z
-        
+
+def get_mse_weighted_loss(predict,y,k=100):
+    y = y[:,1:,:] # (batch, seq, dim_pose) 避開第一章，因為第一個pose為基本已知
+    #有加權過的權重loss計算
+    angle_loss       = torch.nn.functional.mse_loss(predict[:,:,:3], y[:,:,:3])
+    translation_loss = torch.nn.functional.mse_loss(predict[:,:,3:], y[:,:,3:])
+    print(angle_loss , translation_loss)
+    loss = (k * angle_loss + translation_loss)  # k = 100是paper所說的
+    return loss
+
 def test():
 
     torch.manual_seed(1214)
@@ -103,125 +112,33 @@ def test():
     # batch * frame * RGB * img_w , img_h
     x = torch.randn(batch_size,frame,3,img_w,img_h)
     y = torch.randn(batch_size,frame,6) # pose 是 6 維度
+    print(x.shape,y.shape)
 
     MyDeep = myDeepVo(img_w,img_h,frame=frame,hidden_size=1000,n_layers=2)
-    print(MyDeep)
+    #print(MyDeep)
     
-    predict = MyDeep(x)
-    print(predict , predict.shape)
+    optimizer = torch.optim.Adam(MyDeep.parameters(), lr=0.001, betas=(0.9, 0.999))
+    for i in range(3):
+        MyDeep.train()
+        
+        optimizer.zero_grad()
+
+        predict = MyDeep(x)
+        loss = get_mse_weighted_loss(predict,y)
+        
+        print(predict.shape , loss) #print(predict)
+
+        loss.backward()
+        optimizer.step()
+
+    MyDeep.eval()
+    
+    with torch.no_grad():
+        predict = MyDeep(x)
+        loss = get_mse_weighted_loss(predict,y)
+
+    print(predict.shape , loss) #print(predict)
 
 if __name__ == '__main__':
     test()
-
     
-
-class DeepVO(nn.Module):
-    def __init__(self, imsize1, imsize2, batchNorm=True):
-        super(DeepVO,self).__init__()
-        # CNN
-        self.batchNorm = batchNorm
-        self.clip = par.clip
-        self.conv1   = conv(self.batchNorm,   6,   64, kernel_size=7, stride=2, dropout=par.conv_dropout[0])
-        self.conv2   = conv(self.batchNorm,  64,  128, kernel_size=5, stride=2, dropout=par.conv_dropout[1])
-        self.conv3   = conv(self.batchNorm, 128,  256, kernel_size=5, stride=2, dropout=par.conv_dropout[2])
-        self.conv3_1 = conv(self.batchNorm, 256,  256, kernel_size=3, stride=1, dropout=par.conv_dropout[3])
-        self.conv4   = conv(self.batchNorm, 256,  512, kernel_size=3, stride=2, dropout=par.conv_dropout[4])
-        self.conv4_1 = conv(self.batchNorm, 512,  512, kernel_size=3, stride=1, dropout=par.conv_dropout[5])
-        self.conv5   = conv(self.batchNorm, 512,  512, kernel_size=3, stride=2, dropout=par.conv_dropout[6])
-        self.conv5_1 = conv(self.batchNorm, 512,  512, kernel_size=3, stride=1, dropout=par.conv_dropout[7])
-        self.conv6   = conv(self.batchNorm, 512, 1024, kernel_size=3, stride=2, dropout=par.conv_dropout[8])
-        # Comput the shape based on diff image size
-        __tmp = Variable(torch.zeros(1, 6, imsize1, imsize2))
-        __tmp = self.encode_image(__tmp)
-
-        # RNN
-        self.rnn = nn.LSTM(
-                    input_size=int(np.prod(__tmp.size())), 
-                    hidden_size=par.rnn_hidden_size, 
-                    num_layers=2, 
-                    dropout=par.rnn_dropout_between, 
-                    batch_first=True)
-        self.rnn_drop_out = nn.Dropout(par.rnn_dropout_out)
-        self.linear = nn.Linear(in_features=par.rnn_hidden_size, out_features=6)
-
-        # Initilization
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Linear):
-                kaiming_normal_(m.weight.data)
-                if m.bias is not None:
-                    m.bias.data.zero_()
-            elif isinstance(m, nn.LSTM):
-                # layer 1
-                kaiming_normal_(m.weight_ih_l0)  #orthogonal_(m.weight_ih_l0)
-                kaiming_normal_(m.weight_hh_l0)
-                m.bias_ih_l0.data.zero_()
-                m.bias_hh_l0.data.zero_()
-                # Set forget gate bias to 1 (remember)
-                n = m.bias_hh_l0.size(0)
-                start, end = n//4, n//2
-                m.bias_hh_l0.data[start:end].fill_(1.)
-
-                # layer 2
-                kaiming_normal_(m.weight_ih_l1)  #orthogonal_(m.weight_ih_l1)
-                kaiming_normal_(m.weight_hh_l1)
-                m.bias_ih_l1.data.zero_()
-                m.bias_hh_l1.data.zero_()
-                n = m.bias_hh_l1.size(0)
-                start, end = n//4, n//2
-                m.bias_hh_l1.data[start:end].fill_(1.)
-
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-
-
-    def forward(self, x): 
-        # x: (batch, seq_len, channel, width, height)
-        # stack_image
-        x = torch.cat(( x[:, :-1], x[:, 1:]), dim=2)
-        batch_size = x.size(0)
-        seq_len = x.size(1)
-        # CNN
-        x = x.view(batch_size*seq_len, x.size(2), x.size(3), x.size(4))
-        x = self.encode_image(x)
-        x = x.view(batch_size, seq_len, -1)
-
-
-        # RNN
-        out, hc = self.rnn(x)
-        out = self.rnn_drop_out(out)
-        out = self.linear(out)
-        return out
-        
-
-    def encode_image(self, x):
-        out_conv2 = self.conv2(self.conv1(x))
-        out_conv3 = self.conv3_1(self.conv3(out_conv2))
-        out_conv4 = self.conv4_1(self.conv4(out_conv3))
-        out_conv5 = self.conv5_1(self.conv5(out_conv4))
-        out_conv6 = self.conv6(out_conv5)
-        return out_conv6
-
-    def weight_parameters(self):
-        return [param for name, param in self.named_parameters() if 'weight' in name]
-
-    def bias_parameters(self):
-        return [param for name, param in self.named_parameters() if 'bias' in name]
-
-    def get_loss(self, x, y):
-        predicted = self.forward(x)
-        y = y[:, 1:, :]  # (batch, seq, dim_pose) 避開第一章，因為第一個pose為基本已知
-        # Weighted MSE Loss
-        angle_loss = torch.nn.functional.mse_loss(predicted[:,:,:3], y[:,:,:3])
-        translation_loss = torch.nn.functional.mse_loss(predicted[:,:,3:], y[:,:,3:])
-        loss = (100 * angle_loss + translation_loss)
-        return loss
-
-    def step(self, x, y, optimizer):
-        optimizer.zero_grad()
-        loss = self.get_loss(x, y)
-        loss.backward()
-        if self.clip != None:
-            torch.nn.utils.clip_grad_norm(self.rnn.parameters(), self.clip)
-        optimizer.step()
-        return loss
